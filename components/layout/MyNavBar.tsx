1e85747f4fbb83f1a960a1fad4ea7c0dfc0f7c8e
singwithaashish <singwithaashish@gmail.com>
2023-06-18 11:04:29 +0530

    mobile nav responsive and colored

--- components/layout/MyNavBar.tsx
+++ components/layout/MyNavBar.tsx
@@ -1,28 +1,65 @@
-import { Box, Button, Nav } from "grommet";
+import { Box, Button, Nav, ResponsiveContext } from "grommet";
 import { Clock, List, Dashboard, SettingsOption } from "grommet-icons";
 import Link from "next/link";
 
 function MyNavBar() {
+  const currentPath =
+    typeof window !== "undefined" ? window.location.pathname : "";
   return (
-    <Box direction="row" pad="small" gap="medium" justify="evenly" align="center" style={{
-        position: "sticky",
-        bottom: "0px",
-        background: "white",
-        width: "100vw"
-    }}>
-      <Link href="/" passHref>
-        <Button icon={<Clock />} hoverIndicator />
-      </Link>
-      <Link href="/tasks" passHref>
-        <Button icon={<List />} hoverIndicator />
-      </Link>
-      <Link href="/dashboard" passHref>
-        <Button icon={<Dashboard />} hoverIndicator />
-      </Link>
-      <Link href="/settings" passHref>
-        <Button icon={<SettingsOption />} hoverIndicator />
-      </Link>
-    </Box>
+    <ResponsiveContext.Consumer>
+      {(size) =>
+        size === "small" && (
+          <Box
+            direction="row"
+            pad="small"
+            gap="medium"
+            justify="evenly"
+            align="center"
+            style={{
+              position: "sticky",
+              bottom: "0px",
+              background: "white",
+              width: "100vw",
+            }}
+          >
+            <Link href="/" passHref>
+              <Button
+                icon={<Clock color={currentPath === "/" ? "brand" : "gray"} />}
+                hoverIndicator
+              />
+            </Link>
+            <Link href="/tasks" passHref>
+              <Button
+                icon={
+                  <List color={currentPath === "/tasks" ? "brand" : "gray"} />
+                }
+                hoverIndicator
+              />
+            </Link>
+            <Link href="/dashboard" passHref>
+              <Button
+                icon={
+                  <Dashboard
+                    color={currentPath === "/dashboard" ? "brand" : "gray"}
+                  />
+                }
+                hoverIndicator
+              />
+            </Link>
+            <Link href="/settings" passHref>
+              <Button
+                icon={
+                  <SettingsOption
+                    color={currentPath === "/settings" ? "brand" : "gray"}
+                  />
+                }
+                hoverIndicator
+              />
+            </Link>
+          </Box>
+        )
+      }
+    </ResponsiveContext.Consumer>
   );
 }
 
