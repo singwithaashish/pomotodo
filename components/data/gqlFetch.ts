234913fabe0798db888a6bb3c02d3c3980b5784d
singwithaashish <singwithaashish@gmail.com>
2023-06-19 00:39:57 +0530

    implemented graphql

---
+++ components/data/gqlFetch.ts
@@ -0,0 +1,80 @@
+import { gql } from "@apollo/client";
+
+export const allTasks = gql`
+  query {
+    tasks {
+      id
+      title
+      description
+      dueDate
+      tomatoes
+      priority
+      createdAt
+      updatedAt
+      completed
+      timeSpent
+    }
+  }
+`;
+
+export const CREATE_TASK = gql`
+  mutation CreateTask(
+    $title: String!
+    $description: String!
+    $dueDate: DateTime!
+    $priority: String!
+    $tomatoes: Int!
+  ) {
+    createTask(
+      title: $title
+      description: $description
+      dueDate: $dueDate
+      priority: $priority
+      tomatoes: $tomatoes
+    ) {
+      id
+      title
+      description
+      dueDate
+      tomatoes
+      priority
+    }
+  }
+`;
+
+export const UPDATE_TASK = gql`
+    mutation UpdateTask(
+        $id: Int!
+        $title: String!
+        $description: String!
+        $dueDate: DateTime!
+        $priority: String!
+        $tomatoes: Int!
+    ) {
+        updateTask(
+            id: $id
+            title: $title
+            description: $description
+            dueDate: $dueDate
+            priority: $priority
+            tomatoes: $tomatoes
+        ) {
+            id
+            title
+            description
+            dueDate
+            tomatoes
+            priority
+        }
+    }
+`;
+
+export const DELETE_TASK = gql`
+    mutation DeleteTask($id: Int!) {
+        deleteTask(id: $id) {
+            id
+        }
+    }
+`;
+
+
